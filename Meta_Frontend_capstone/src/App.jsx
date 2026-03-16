2ebdadc2d30e4ac08780b063e47e068aab8e96d4
Kyle Frank <kyle.frankenfield@gmail.com>
2026-03-16 13:57:28 -0400

    initial commit

---
+++ Meta_Frontend_capstone/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>This is my Meta Capstone</p>
+      <p>It's going to be siiiick!</p>
+    </>
+  )
+}
+
+export default App
