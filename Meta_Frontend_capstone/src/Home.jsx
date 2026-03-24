05817faab58e8d4a81242feb3d88399706dc500e
Kyle Frank <kyle.frankenfield@gmail.com>
2026-03-24 10:52:45 -0400

    About section styled and review pictures fixed

--- Meta_Frontend_capstone/src/Home.jsx
+++ Meta_Frontend_capstone/src/Home.jsx
@@ -37,10 +37,26 @@ const Home =()=>{
                 <h3>What do our customers think?</h3>
                 <div className="reviewsContainer">
                     <Review pic="saraLopez.jpg" name="Sara Lopez" username="Sara72" text='"Seriously cannot stop thinking about the Turkish Mac n Cheese!!"'/>
-                     <Review pic="johnDoucette.jpg" name="John Doucette" username="Johnny_Utah" text='"We had such a great time celebrating my grandmothers birthday!"'/>
-                      <Review pic="jimmyCrickets.jpg" name="Jimmy Crickets" username="crick_it" text='"Such a chilled out atmosphere, love it!"'/>
-                       <Review pic="miaMaria.jpg" name="Mia Maria" username="flowerTime" text='"Best Feta Salad in town. Flawless every time!"'/>
+                    <Review pic="johnDoucette.jpg" name="John Doucette" username="Johnny_Utah" text='"We hadsuch a great time celebrating my grandmothers birthday!"'/>
+                    <Review pic="jimmyCrickets2.jpg" name="Jimmy Crickets" username="crick_it" text='"Such achilled out atmosphere, love it!"'/>
+                    <Review pic="miaMaria.jpg" name="Mia Maria" username="flowerTime" text='"Best Feta Salad in town. Flawless every time!"'/>
                 </div>
+            </section>
+            <section className="aboutSection twelveColGrid">
+                <div className="aboutInfo">
+                    <div>
+                        <div className="aboutTitle">
+                            <h2>Little Lemon</h2>
+                            <h3>Chicago</h3>
+                        </div>
+                        <p>Little Lemon Chicago is a family owned Mediterranean restaurant located in the heart of the city. The restaurant is run by brothers Mario and Adrian, who have always had a passion for cooking and serving delicious food. Growing up in a Mediterranean household, the brothers were exposed to traditional recipes from an early age, and they decided to bring those recipes to the masses with a modern twist. At Little Lemon, you can expect to find a menu full of classic dishes with a creative twist that makes them stand out from the rest. Whether you're looking for a quick lunch or a leisurely dinner, Little Lemon Chicago is the perfect place to indulge in a delicious meal in a cozy and welcoming atmosphere.</p>
+                    </div>
+                </div>
+                <div className="aboutImgContainer">
+                    <div className="aboutImg" id="aboutTwo"></div>
+                    <div className="aboutImg" id="aboutOne"></div>
+                </div>
+
             </section>
         </>
     )
